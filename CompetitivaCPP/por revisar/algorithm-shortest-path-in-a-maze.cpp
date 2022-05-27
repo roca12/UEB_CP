ba52b3cbed3cb05f97a02f3c2f2ed1eca16ac2ee
Edwin Andres Villarraga Ardila <62625858+edanv1401@users.noreply.github.com>
2022-05-27 15:56:38 -0500

    Update algorithm-shortest-path-in-a-maze.cpp

--- CompetitivaCPP/por revisar/algorithm-shortest-path-in-a-maze.cpp
+++ CompetitivaCPP/por revisar/algorithm-shortest-path-in-a-maze.cpp
@@ -1,6 +1,9 @@
 // Source
 // https://www.techiedelight.com/lee-algorithm-shortest-path-in-a-maze/
 
+// testing 
+// 11624 - Fire! Online Judge
+
 #include <iostream>
 #include <queue>
 #include <vector>
