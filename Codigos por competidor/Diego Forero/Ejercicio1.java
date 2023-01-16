a804062fbef113de569c18223025840b9823aaed
DiegoF1311 <67521270+DiegoF1311@users.noreply.github.com>
2023-01-16 16:47:28 -0500

    Create Ejercicio1.java

---
+++ Codigos por competidor/Diego Forero/Ejercicio1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ProblemA {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for (int j = 0; j < t; j++) {
+			int n = sc.nextInt(), k = sc.nextInt();
+			String res = "";
+			
+			for (int i = 0; i < n; i++) {
+				int a = sc.nextInt();
+				if (a <= k) {
+					k = k - a;
+					res = res + "1";
+				} else if (a > k) {
+					res = res + "0";
+				}
+			}
+			System.out.println(res);
+		}
+
+	}
+
+}
