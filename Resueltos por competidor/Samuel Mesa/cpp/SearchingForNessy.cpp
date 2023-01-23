4024ab6f8ae08f869a88239c2c28c3b6a842160b
JUSTSAM3 <119260205+JUSTSAM3@users.noreply.github.com>
2023-01-23 16:31:03 -0500

    :D

---
+++ Resueltos por competidor/Samuel Mesa/cpp/SearchingForNessy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int t;
+	cin>>t;
+	for(int i =0;i<t;i++){
+		int a,b;
+		cin>>a;
+		cin>>b;
+		cout<<(a / 3) * (b / 3)<<endl;
+	}
+	return 0;
+}
+
