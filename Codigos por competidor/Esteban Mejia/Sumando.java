c605cedb19b03fa72b4f6dcedc5b21565067747e
3xiic <122654327+3xiic@users.noreply.github.com>
2023-01-25 01:09:08 -0500

    Add files via upload

---
+++ Codigos por competidor/Esteban Mejia/Sumando.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Sumando {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int t= sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt(),d=sc.nextInt();
+			System.out.println(a+b+c+d);
+		}
+	}
+}
