409505cef3985135612015f2003f5a96d5e9e28d
Yishaq Riveros <91137117+Yishaqriv@users.noreply.github.com>
2023-02-16 18:30:27 -0500

    Add files via upload

---
+++ Resueltos por competidor/Yishaq Riveros/DOMjudge/MainHelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MainHelloWorld {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String loquesea=sc.nextLine();
+		System.out.println("Hello world!");
+	}
+
+}
