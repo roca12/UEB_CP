35107e56480064bbc477a3cf9516d28d8170161b
JUSTSAM3 <119260205+JUSTSAM3@users.noreply.github.com>
2023-02-18 17:16:27 -0500

    Add files via upload

---
+++ Resueltos por competidor/Samuel Mesa/cpp/The Snail.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int h,u,d,f,dia;
+	float sube,pos;
+	bool win;
+	
+	
+	while(cin>>h && h!=0){
+		cin>>u>>d>>f;
+		
+		win = true;
+		
+		dia = 1;
+		pos = 0;
+		
+		sube = u;
+		float fati = (f * sube) / 100;
+		
+		while (true) {
+			
+			if (fati > 0) {pos += sube;}
+			if (pos > h) {break;}
+			pos -= d;
+			sube = sube - fati;
+			if (pos < 0) {win = false;break;}
+			
+			dia++;
+		}
+		win ? cout<<"success on day "<<dia<<endl : cout<<"failure on day "<<dia<<endl;
+	}
+	return 0;
+}
