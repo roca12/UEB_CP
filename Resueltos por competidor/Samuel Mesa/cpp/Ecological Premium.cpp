67e514c5337ed82ca965b6e7647c1ce920f7a705
JUSTSAM3 <119260205+JUSTSAM3@users.noreply.github.com>
2023-02-27 22:21:34 -0500

    Add files via upload

---
+++ Resueltos por competidor/Samuel Mesa/cpp/Ecological Premium.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int casos;
+	cin>>casos;
+	for(int i =0;i<casos;i++){
+		
+		int granjeros,prima=0;
+		cin>>granjeros;
+		
+		for(int j =0;j<granjeros;j++){
+			
+			int tama,animales,grado,aux;
+			
+			cin>>tama>>animales>>grado;
+			
+			//aux =((tama/animales)*grado)*animales;
+			aux = tama*grado;
+			prima= prima +aux;
+		}
+		cout<<prima<<endl;
+	}
+	
+	
+	return 0;
+}
+
