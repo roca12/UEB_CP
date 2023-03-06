2cabc529c41bdf2ea21ada803f79007c39830cf0
Mgonzal3z <81936645+Mgonzal3z@users.noreply.github.com>
2023-03-05 19:20:07 -0500

    Add files via upload

---
+++ Codigos por competidor/Miguel Angel Gonzalez Vargas/Hello_World.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		if(num == 1) {
+			System.out.println("Hello world!");
+		}
+		
+	}
+
+}
