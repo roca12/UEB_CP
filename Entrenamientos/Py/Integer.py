078892bdd86f51c8cdfb7ff99afab602447214ba
Diego Rodriguez <44822667+roca12@users.noreply.github.com>
2023-03-06 19:57:38 -0500

    060323

---
+++ Entrenamientos/Py/Integer.py
@@ -0,0 +1,6 @@
+numero =int(input())
+resultado=0
+while (numero!=0):
+    resultado=resultado+numero
+    numero = int(input())
+print(resultado)
---
+++ Entrenamientos/src/Integer.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Integer {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		BigInteger entrada= sc.nextBigInteger();
+		BigInteger resultado= new BigInteger("0");
+		while(!entrada.equals(BigInteger.ZERO)) {
+			resultado=resultado.add(entrada);
+			entrada= sc.nextBigInteger();
+		}
+		System.out.println(resultado);
+	}
+
+}
