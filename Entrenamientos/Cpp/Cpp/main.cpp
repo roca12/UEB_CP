c36c2596ba915e1302d826e99c6bbd1af859b49b
Diego Rodriguez <44822667+roca12@users.noreply.github.com>
2023-03-14 20:02:42 -0500

    140323

---
+++ Entrenamientos/Cpp/Cpp/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h> //libreria GPC
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int t;
+	cin>>t; //lector.nextInt();
+	for(int i=0; i<t;i++){
+		int n1,n2,n3,n4;
+		cin>>n1>>n2>>n3>>n4;
+		int res=n1+n2+n3+n4;
+		cout<<res<<endl;
+	}
+	return 0;
+}
+
--- Entrenamientos/Py/Integer.py
+++
@@ -1,6 +0,0 @@
-numero =int(input())
-resultado=0
-while (numero!=0):
-    resultado=resultado+numero
-    numero = int(input())
-print(resultado)
