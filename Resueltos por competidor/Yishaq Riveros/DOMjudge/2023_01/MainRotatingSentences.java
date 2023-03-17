6e2ca6f982360b47b13f6dada31810b2a59e94aa
Yishaq Riveros <91137117+Yishaqriv@users.noreply.github.com>
2023-03-17 14:15:41 -0500

    Add files via upload

---
+++ Resueltos por competidor/Yishaq Riveros/DOMjudge/2023_01/MainRotatingSentences.java
@@ -0,0 +1,32 @@
+package co.edu.unbosque;
+
+import java.util.Scanner;
+
+public class MainRotatingSentences {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] sentences = new String[100];
+        int maxLength = 0;
+
+        // Read input sentences and find the maximum length
+        for (int i = 0; i < 100 && scanner.hasNextLine(); i++) {
+            String sentence = scanner.nextLine();
+            sentences[i] = sentence;
+            maxLength = Math.max(maxLength, sentence.length());
+        }
+
+        // Print rotated sentences
+        for (int i = 0; i < maxLength; i++) {
+            for (int j = sentences.length - 1; j >= 0; j--) {
+                String sentence = sentences[j];
+                if (sentence != null && i < sentence.length()) {
+                    System.out.print(sentence.charAt(i));
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
