2c8a4e9f88da1899d4fa2a8281e08839002a6265
Diego Rodriguez <44822667+roca12@users.noreply.github.com>
2023-09-26 13:51:57 -0500

    26092023

--- Entrenamientos/2023-2 nuevo ciclo/src/Estructuras.java
+++ Entrenamientos/2023-2 nuevo ciclo/src/Estructuras.java
@@ -1,41 +1,88 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Random;
+import java.util.Stack;
 
 public class Estructuras {
 
 	public static void main(String[] args) {
-		// arrays
-		Random  r = new Random(55556L);
-		
-		int [] array= new int[10];
-		for (int i = 0; i < array.length; i++) {
-			array[i]=r.nextInt(1000);
+
+		Random r = new Random();
+
+		LinkedList<Character> listaDobleLazo = new LinkedList<>();
+		for (int i = 0; i < 15; i++) {
+			// ascii
+			char c = (char) r.nextInt(127);
+			if (Character.isAlphabetic(c)) {
+				listaDobleLazo.addFirst(c);
+			} else if (Character.isDigit(c)) {
+				listaDobleLazo.addLast(c);
+			} else {
+				i--;
+			}
 		}
-		for (int j = 0; j < array.length; j++) {
-			System.out.println(j+" -> "+array[j]);
+		//lambda
+		listaDobleLazo.forEach((c) -> {
+			System.out.print(c);
+			System.out.print(" ");
+		});
+		System.out.println();
+		
+		
+		Queue<Character> cola = new LinkedList<Character>();
+		for (int i = 0; i < 7; i++) {
+			char c=(char) r.nextInt(127);
+			cola.offer(c); 
 		}
-		
-		int[][] matrix = new int[5][5];
-		
-		Arrays.binarySearch(array, 64);
-		Arrays.fill(array, 0);
-		System.out.println(Arrays.toString(array));
-		
-		
-		//arraylist
-		//envoltorio
-		List<Double> lista= new ArrayList<>();
-		for (int i = 0; i < 10; i++) {
-			lista.add(r.nextGaussian());
+		System.out.println(cola.toString());
+		while(!cola.isEmpty()) {
+			System.out.println("El char que esta al frente es "+cola.peek());
+			System.out.println("Se va:" + cola.poll());
 		}
-		for (int i = 0; i < lista.size(); i++) {
-			System.out.println(i+" -> "+lista.get(i));
-		}
+		System.out.println("Cola vacia");
 		
-		ArrayList<ArrayList<Long>> listadelistas= new ArrayList<>();
+		System.out.println("///////////////////////");
+		
+		Stack<Character> pila = new Stack<Character>();
+		for (int i = 0; i < 7; i++) {
+			char c=(char) r.nextInt(127);
+			pila.push(c); 
+		}
+		System.out.println(pila.toString());
+		while(!pila.isEmpty()) {
+			System.out.println("El char que esta arriba es "+pila.peek());
+			System.out.println("Se va:" + pila.pop());
+		}
+		System.out.println("Pila vacia");
+		
+
+//		int [] array= new int[10];
+//		for (int i = 0; i < array.length; i++) {
+//			array[i]=r.nextInt(1000);
+//		}
+//		for (int j = 0; j < array.length; j++) {
+//			System.out.println(j+" -> "+array[j]);
+//		} 
+//		
+//		int[][] matrix = new int[5][5];
+//		
+//		Arrays.binarySearch(array, 64);
+//		Arrays.fill(array, 0);
+//		System.out.println(Arrays.toString(array));
+//		
+//		//arraylist
+//		List<Double> lista= new ArrayList<>();
+//		for (int i = 0; i < 10; i++) {
+//			lista.add(r.nextGaussian());
+//		}
+//		for (int i = 0; i < lista.size(); i++) {
+//			System.out.println(i+" -> "+lista.get(i));
+//		}
+//		ArrayList<ArrayList<Long>> listadelistas= new ArrayList<>();
 
 	}
 
