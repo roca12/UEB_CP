9f43cdb1632691255e21964ae6461e6543d7c710
Jairo Esteban <jairodavid.estebanlozano@gmail.com>
2026-04-23 16:49:28 -0500

    Add files via upload

---
+++ Codigos por competidor/Jairo Esteban/flipflops.java
@@ -0,0 +1,43 @@
+//2209A
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class flipflops {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+            long c = sc.nextLong();
+            long k = sc.nextLong();
+
+            long [] a = new long[n];
+
+            for (int i = 0; i < n; i++){
+                a[i] = sc.nextLong();
+            }
+
+            Arrays.sort(a);
+
+              for (int i = 0; i < n; i++) {
+                if (a[i] > c) break;
+
+                long usar = Math.min(k, c - a[i]); 
+                a[i] += usar;
+                k -= usar;
+
+                c += a[i];
+            }
+
+            System.out.println(c);
+
+        }
+
+    }
+
+}
