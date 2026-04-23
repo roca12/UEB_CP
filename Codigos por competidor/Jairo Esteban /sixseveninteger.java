f7eb6f4a746f9e4614f14ca39421410f193dad53
Jairo Esteban <jairodavid.estebanlozano@gmail.com>
2026-04-22 23:02:37 -0500

    Create sixseveninteger.java

---
+++ Codigos por competidor/Jairo Esteban /sixseveninteger.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//2218A
+public class sixseveninteger {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int x = sc.nextInt();
+
+            if (x < 67) {
+                System.out.println(x + 1);
+            } else {
+                System.out.println(x);
+            }
+
+        }
+
+    }
+
+}
