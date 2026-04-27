4b1bd6952d97fdae7c30e8e27cf4fcfbe52236e6
Tomas <tomastrianag@gmail.com>
2026-04-27 14:19:02 -0500

    NumberSpiral.cpp

---
+++ Resueltos por competidor/Tomás Triana Galvis/NumberSpiral.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+ /*
+ * Autor: Tomás Triana Galvis
+ * Problema: Number Spiral
+ * Juez online: CSES 16755425
+ * Veredicto: Accepted
+ * Url: https://cses.fi/problemset/result/16755425/
+ **/
+using namespace std;
+ 
+int main()
+{
+    long long t, x,y;
+    cin>>t;
+    for(long long i=0; i<t;i++){
+        cin>>x>>y;
+        long long ans = 0;
+        if(x>y){
+            ans+=(x*x)-(x-1);
+            if(x%2==1){
+                ans-=x-y;
+            }else{
+                ans+=x-y;
+            }
+        }else{
+            ans+=(y*y)-(y-1);
+            if(y%2==1){
+                ans+=y-x;
+            }else{
+                ans-=y-x;
+            }
+        }
+        cout<<ans<<endl;
+ 
+    }
+    return 0;
+}
