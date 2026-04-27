8b872596985583bdd00ab7f800ed7fdf14a878fa
Tomas <tomastrianag@gmail.com>
2026-04-27 13:38:30 -0500

    CountBalls.cpp

---
+++ Resueltos por competidor/Tomás Triana Galvis/CountBalls.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+/*
+ * Autor: Tomás Triana Galvis
+ * Problema: Count Balls
+ * Juez online: AtCoder abc158_b
+ * Veredicto: Accepted
+ * Url: https://atcoder.jp/contests/abc158/tasks/abc158_b
+ **/
+using namespace std;
+
+int main()
+{
+    long long n, a, b;
+    cin>>n>>a>>b;
+    long long ans = 0;
+
+    ans+=(n/(a+b))*a;
+    ans+=min(n%(a+b),a);
+    cout<<ans<<endl;
+
+    return 0;
+}
