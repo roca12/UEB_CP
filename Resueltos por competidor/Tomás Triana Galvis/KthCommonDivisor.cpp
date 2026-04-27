5784cf788e6ad401c6a6c43d11eaa09967736ac0
Tomas <tomastrianag@gmail.com>
2026-04-27 14:23:09 -0500

    KthCommonDivisor.cpp

---
+++ Resueltos por competidor/Tomás Triana Galvis/KthCommonDivisor.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+/*
+ * Autor: Tomás Triana Galvis
+ * Problema: K-th Common Divisor
+ * Juez online: AtCoder abc120_b
+ * Veredicto: Accepted
+ * Url: https://atcoder.jp/contests/abc120/tasks/abc120_b
+ **/
+using namespace std;
+
+int main()
+{
+    long long a,b,k;
+    cin>>a>>b>>k;
+    long long x = __gcd(a,b);
+    vector<long long> arr;
+    for(long long i=1;i<=x;i++){
+        if(x%i==0){
+            arr.push_back(i);
+        }
+    }
+    sort(arr.begin(),arr.end());
+    cout<<(arr[arr.size()-k]);
+    return 0;
+}
