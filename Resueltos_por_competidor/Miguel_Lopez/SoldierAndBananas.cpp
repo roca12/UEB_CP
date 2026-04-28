01d31aec041a4258146a6b340b39c018378b9d39
Fetchnt <delsinrowe752@gmail.com>
2026-04-28 17:54:14 -0500

    Add files via upload

---
+++ Resueltos_por_competidor/Miguel_Lopez/SoldierAndBananas.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    typedef long long int ll;
+    int n, k, w, total = 0, debe; cin >> n >> k >> w;
+    for(int i = 1 ; i <= w ; i++) {
+        int op = i * n;
+    //    cout << op << endl;
+        total +=op;
+    }
+    if(total <= k) {
+        cout << 0 << endl;
+    }
+    else {
+        debe = total - k;
+        cout << debe << endl;
+    }
+
+}
