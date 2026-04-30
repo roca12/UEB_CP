7a10b7cb623588dba731f7783d841cb5e57f9d75
Jean Quintero <jpquinterop08@gmail.com>
2026-04-30 10:29:32 -0500

    Add files via upload

---
+++ Resueltos_por_competidor/Jean_Paul/Main6_7.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main6_7 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int t = sc.nextInt();
+		for (int j = 0; j < t; j++) {
+
+			int sixseven[] = new int[7];
+			for (int i = 0; i < sixseven.length; i++) {
+				sixseven[i] = sc.nextInt();
+			}
+
+			Arrays.sort(sixseven);
+			int sum = 0;
+			for (int i = 0; i < sixseven.length; i++) {
+				if (i != 6)
+					sum = sum -sixseven[i];
+				else {
+					sum += sixseven[i];
+				}
+			}
+
+			System.out.println(sum);
+		}
+	}
+	
+	/*
+	 *  Autor: Jean Paul
+	 * Problema: B. The 67th 6-7 Integer Problem
+	 * Juez online: Polygon 
+	 * Veredicto: AC
+	 * URL: https://codeforces.com/problemset/problem/2218/B
+	 */
+}
