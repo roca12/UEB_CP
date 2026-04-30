ae5c701f43433118ef943487c1a8ee7e04f0b14d
DynaMightt327 <malejacarvajalnepta@gmail.com>
2026-04-30 15:29:49 -0500

    Subscriptions in python

---
+++ Resueltos_por_competidor/Alejandra_Carvajal/subscriptions.py
@@ -0,0 +1,11 @@
+import math
+
+t = int(input())
+
+for _ in range(t):
+    n, x = map(int, input().split())
+    
+    grupo_de_seis = math.ceil(n / 6)
+    precio_final = x * grupo_de_seis
+    
+    print(precio_final)
