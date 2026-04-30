ffe2a69e2dabfbd7faf4e6af6641519ac4c953ea
Gabriella Castro <159205699+gabGABY006@users.noreply.github.com>
2026-04-29 21:34:28 -0500

    Create C23.java

---
+++ Resueltos_por_competidor/Gabriella_Castro/C23.java
@@ -0,0 +1,27 @@
+/*
+ * Autor: Gabriella Castro
+ * Problema: C23
+ * Juez online: OmegaUp
+ * Veredicto: Accepted / Correct / Yes / Ok
+ * URL: https://omegaup.com/arena/problem/C23/
+ */
+
+import java.util.Scanner;
+
+public class C23 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int i = sc.nextInt();
+		
+		if(i % 2 == 0) {
+			System.out.println("par");
+		}else {
+			System.out.println("impar");
+		}
+		
+	}
+
+}
