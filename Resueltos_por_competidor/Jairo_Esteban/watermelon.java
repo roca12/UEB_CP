d717ca4faacea5b5897ec931f15c85e75fb0960a
Jairo Esteban <jairodavid.estebanlozano@gmail.com>
2026-05-02 10:40:00 -0500

    Add header comments to watermelon.java
    Added comments with author, problem details, and URL.

--- Resueltos_por_competidor/Jairo_Esteban/watermelon.java
+++ Resueltos_por_competidor/Jairo_Esteban/watermelon.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 //1
+/*
+ * Autor: [kjairoo]
+ * Problema: [Watermelon]
+ * Juez online: [Codeforce]
+ * Veredicto: Accepted
+ * URL: [https://codeforces.com/problemset/problem/4/A]
+*/
 public class watermelon {
 
     static Scanner sc = new Scanner(System.in);
