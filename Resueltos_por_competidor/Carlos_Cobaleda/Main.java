6a021178abab380f7885d15cb40d28112301ef5b
Shinrra_132K <124542111+Shinrra-132K@users.noreply.github.com>
2026-05-01 23:02:04 -0500

    Add files via upload

---
+++ Resueltos_por_competidor/Carlos_Cobaleda/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			
+			int r1 = sc.nextInt();
+			int r2 = sc.nextInt();
+			double h = sc.nextDouble();
+			double p = sc.nextDouble();
+			double ph = p/h;
+			double rsl = r2 + ((r1- r2) * ph);
+			double srs = (rsl*rsl) + (r2*r2) + (rsl*r2);
+			double v = (Math.PI/3) * srs * p;
+			System.out.println("Case " + (i+1) + ":" + " "+ v);
+			
+		}
+		
+	}
+
+}
