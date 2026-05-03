57f73c3f0f38307f9547d0333c0e2955794ffec3
DavsAlejo <dvelasquezs@unbosque.edu.co>
2026-05-02 19:00:36 -0500

    Add Borze problem solution in b-borze.java

---
+++ Resueltos_por_competidor/David_Velasquez/b-borze.java
@@ -0,0 +1,39 @@
+/*
+* Autor: DavsAlejo
+* Problema: B - Borze
+* Juez Online: Codeforces
+* Veredicto: Accepted
+* URL: https://codeforces.com/contest/32/problem/B
+*/
+
+import java.util.Scanner;
+
+public class Borze {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		String borzeCode = sc.next();
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < borzeCode.length(); i++) {
+			char current = borzeCode.charAt(i);
+
+			if (current == '.') {
+				result.append("0");
+			} else if (current == '-') {
+
+				if (borzeCode.charAt(i + 1) == '.') {
+					result.append("1");
+				} else {
+					result.append("2");
+				}
+				i++;
+			}
+		}
+
+		System.out.println(result.toString());
+
+	}
+}
