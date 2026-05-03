1f5df3f954fb0bd2c5a7643331e409623412733a
SayisC <sjrincon@unbosque.edu.co>
2026-05-03 16:13:20 -0500

    Create Minimize!.cpp

---
+++ Codigos_por_competidor/Sahian_Castro/Minimize!.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+#define print(x) cout << x << "\n"
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; 
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        int r = b - a;
+        print(r);
+    }
+}
