502098d630c94c62b788774ffc351fc1dacef45a
SayisC <sjrincon@unbosque.edu.co>
2026-05-03 16:45:00 -0500

    Add comments for problem details in Minimize!.cpp
    Added problem statement and author information as comments.

--- Resueltos_por_competidor/Sahian_Castro/Minimize!.cpp
+++ Resueltos_por_competidor/Sahian_Castro/Minimize!.cpp
@@ -1,3 +1,11 @@
+/*
+Autor : SayisC
+Problema : Minimize!
+Juez online : 
+Veredicto : Accepted
+Url : https://codeforces.com/contest/2009/problem/A
+*/
+
 #include <iostream>
 using namespace std; 
 #define print(x) cout << x << "\n"
