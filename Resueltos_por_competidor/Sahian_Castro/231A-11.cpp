f836f159e89cd8f73a9242716c2ee7e52c31fd9d
SayisC <sjrincon@unbosque.edu.co>
2026-05-03 16:43:48 -0500

    Add header comments and include directive

--- Resueltos_por_competidor/Sahian_Castro/231A-11.cpp
+++ Resueltos_por_competidor/Sahian_Castro/231A-11.cpp
@@ -1,3 +1,11 @@
+/*
+Autor : SayisC
+Problema : Team
+Juez online : 
+Veredicto : Accepted
+Url : https://codeforces.com/contest/231/problem/A
+*/
+
 #include <iostream>
 using namespace std; 
 #define print(x) cout << x << "\n"
