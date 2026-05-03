3994dc6b6a81cab505a6b6d0bcfe0cc8b38a274f
Jean Quintero <jpquinterop08@gmail.com>
2026-05-03 09:03:13 -0500

    Add files via upload

---
+++ Resueltos_por_competidor/Jean_Paul/MainEP.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MainEP {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int t = sc.nextInt();
+		int x;
+		for (int i = 0; i < t; i++) {
+			x = sc.nextInt();
+			System.out.println(x - 1);
+		}
+	}
+	/*
+	 *  Autor: Jean Paul
+	 * Problema: A. Easy Problem
+	 * Juez online: Polygon 
+	 * Veredicto: AC
+	 * URL: https://codeforces.com/problemset/problem/2044/A
+	 */
+}
