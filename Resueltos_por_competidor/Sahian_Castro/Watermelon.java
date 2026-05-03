134621f53d35be94de813fd6deb763730e804271
SayisC <sjrincon@unbosque.edu.co>
2026-05-03 16:33:36 -0500

    Add Watermelon.java to check even weight

---
+++ Resueltos_por_competidor/Sahian_Castro/Watermelon.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+  public class Main{
+    	public static void main(String[] args) {
+    		Scanner sc = new Scanner(System.in);
+    		int w = sc.nextInt();
+    		if(w > 2 && w % 2 == 0){
+    		    System.out.println("Yes");
+    		}else{
+    		    System.out.println("No");
+    		}
+    	}
+    }
