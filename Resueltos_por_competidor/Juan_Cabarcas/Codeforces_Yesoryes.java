3f545cc20aa0e50bd5ce3ebb54900bf6fbed7405
jcabarcasr <jcabarcasr@unbosque.edu.co>
2026-05-04 18:51:18 -0500

    Add files via upload

--- Resueltos_por_competidor/Juan_Cabarcas/Codeforces_Yesoryes.java
+++ Resueltos_por_competidor/Juan_Cabarcas/Codeforces_Yesoryes.java
@@ -4,33 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 /*
  * Autor: Juan Cabarcas 
- * Problema: 71A. Way too long words 
+ * Problema: 1703A. YES OR YES
  * Juez online: codeforces 
  * Veredicto: Accepted
- * URL: https://codeforces.com/problemset/problem/71/A
+ * URL: https://codeforces.com/problemset/problem/1703/A
  */
 
-public class Codeforces_Waytoolongwords {
+public class Codeforces_Yesoryes {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
+
 		for (int i = 0; i < n; i++) {
-			String wol = br.readLine();
-
-			if (wol.length() > 10) {
-				int m = wol.length() - 2;
-				char s = wol.charAt(0);
-				char e = wol.charAt(wol.length() - 1);
-
-				System.out.println("" + s + m + e);
+			String s = br.readLine().toLowerCase();
+			if (s.equals("yes")) {
+				System.out.println("YES");
 			} else {
-				System.out.println(wol);
+				System.out.println("NO");
 			}
-
 		}
-	}
+	} 
 }
