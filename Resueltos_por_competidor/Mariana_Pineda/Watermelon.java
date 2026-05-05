29def3f3768d6f9c4878c9701dabd89913666eba
Mariana Pineda <maju514117@gmail.com>
2026-05-04 22:04:12 -0500

    Create Watermelon.java

---
+++ Resueltos_por_competidor/Mariana_Pineda/Watermelon.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	/*
+	 * Autor: mariana 
+	 * Problema : Watermelon 
+	 * Juez online: codeforces 
+	 * Veredicto: Accepted 
+	 * URL: https://omegaup.com/arena/problem/Watermel/
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n % 2 == 0 && n > 2) {
+			System.out.println("SI");
+		} else {
+			System.out.println("NO");
+		}
+	}
+}
