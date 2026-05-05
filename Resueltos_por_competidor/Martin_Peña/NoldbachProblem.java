12ccd30693ebcb4b03b2c2cf645f607f8a10925c
Si <pgms1103@gmail.com>
2026-05-05 15:10:32 -0500

    Add files via upload

---
+++ Resueltos_por_competidor/Martin_Peña/NoldbachProblem.java
@@ -0,0 +1,61 @@
+/*
+* Autor: pgms1103-afk
+* Problema: Noldbach problem
+* Juez Online: Codeforces
+* Veredicto: Accepted
+* URL: https://codeforces.com/problemset/problem/17/A
+*/
+
+import java.util.Scanner;
+ 
+public class NoldbachProblem {
+ 
+	static Scanner sc = new Scanner(System.in);
+ 
+	public static void main(String[] args) {
+ 
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int contador = 0;
+		int primos[] = new int[n];
+		int pos = 0;
+ 
+		for (int i = 2; i <= n; i++) {
+			
+			boolean primo = true;
+			for (int divisor = 2; divisor <= Math.sqrt(i); divisor++) {
+				if (i % divisor == 0) {
+					primo = false;
+				}
+			}
+			if (primo) {
+				primos[pos] = i;
+				pos++;
+			}
+		}
+		
+		  for (int i = 0; i < pos - 1; i++) {
+	            int suma = primos[i] + primos[i + 1] + 1;
+
+	            if (suma >= 2 && suma <= n) {
+
+	                boolean esPrimo = true;
+	                for (int d = 2; d <= Math.sqrt(suma); d++) {
+	                    if (suma % d == 0) {
+	                        esPrimo = false;
+	                        break;
+	                    }
+	                }
+
+	                if (esPrimo) {
+	                    contador++;
+	                }
+	            }
+	}
+		  if(contador >= k) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
+}
+}
