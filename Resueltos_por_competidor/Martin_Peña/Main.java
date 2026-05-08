33e6316103a066c79d928bce485e3d0e3f0eeb1b
Si <pgms1103@gmail.com>
2026-05-08 08:30:35 -0500

    Add files via upload

--- Resueltos_por_competidor/Martin_Peña/Main.java
+++ Resueltos_por_competidor/Martin_Peña/Main.java
@@ -1,40 +1,46 @@
 /*
 * Autor: pgms1103-afk
-* Problema: Producto punto de dos vectores
+* Problema: Formados en la cafetería
 * Juez Online: omegaUp
 * Veredicto: Accepted
-* URL: https://omegaup.com/arena/problem/Producto-punto-de-dos-vectores/#problems
+* URL: https://omegaup.com/arena/problem/Formados-en-la-cafeteria/#problems
 */
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
-	static Scanner sc = new Scanner (System.in);
-	public static void main(String[] args) {
-		
-		int n = sc.nextInt();
-		int resultado = 0;
-		int contador = 0;
-		int a[] = new int[n];
-		int b[] = new int[n];
-		
-		for(int i = 0; i<n; i++) {
-			a[i] = sc.nextInt();
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		int t = Integer.parseInt(br.readLine());
+		Queue<String> alumnos = new LinkedList<>();
+		Queue<String> trabajadores = new LinkedList<>();
+		for (int i = 0; i < t; i++) {
+			
+			String evento = br.readLine();
+
+
+			if (evento.contains("ALUMNO")) {
+				String nombreAlumno = evento.substring(13);
+				alumnos.offer(nombreAlumno);
+			} else if (evento.contains("TRABAJADOR")) {
+				String nombreTrabajador = evento.substring(17);
+				trabajadores.offer(nombreTrabajador);
+			}else {
+				if(trabajadores.isEmpty()){
+					String estudianteAtendido = alumnos.poll();
+					System.out.println(estudianteAtendido);
+				}else {
+					String trabajadorAtendido = trabajadores.poll();
+					System.out.println(trabajadorAtendido);
+				}
+			}
 		}
-	
-		for(int i = 0; i<n; i++) {
-			b[i] = sc.nextInt();
-		}
-		
-		while(contador < n) {
-			resultado = a[contador] * b[contador] + resultado;	
-			contador++;
-		}
-		System.out.println(resultado);
-		
-		
-		
 
 	}
 
