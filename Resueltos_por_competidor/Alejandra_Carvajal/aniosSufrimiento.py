31f95a7ac73109a20abea427b1aa8a3e9fd6b7a2
DynaMightt327 <malejacarvajalnepta@gmail.com>
2026-05-11 20:07:43 -0500

    suffering calculation

---
+++ Resueltos_por_competidor/Alejandra_Carvajal/aniosSufrimiento.py
@@ -0,0 +1,14 @@
+# Autor: Alejandra Carvajal
+# Problema: Años de sufrimiento
+# Juez online: OmegaUp
+# Veredicto: Accepted
+# URL: https://omegaup.com/arena/problem/Anos-de-sufrimiento/
+
+y = int(input())
+
+year = 365;
+day = 24;
+hour = 60;
+minute = 60;
+
+print(y * year * day * hour * minute)
