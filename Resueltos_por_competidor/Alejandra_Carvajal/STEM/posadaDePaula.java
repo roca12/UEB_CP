8be433d398f6da94caea3098f28e19329d7fec3e
DynaMightt327 <malejacarvajalnepta@gmail.com>
2026-05-11 20:10:17 -0500

    Add posadaDePaula.java

---
+++ Resueltos_por_competidor/Alejandra_Carvajal/STEM/posadaDePaula.java
@@ -0,0 +1,24 @@
+/* Autor: Alejandra Carvajal
+* Problema: Posada de Paula
+* Juez online: OmegaUp
+* Veredicto: Accepted
+* URL: 
+*/
+
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[] familias = new int[N]; 
+        int totalFamilias = 0;
+
+        for (int i = 0; i < N; i++) {
+            familias[i] = scanner.nextInt(); 
+            totalFamilias += familias[i];
+        }
+
+        System.out.println(totalFamilias);
+
+        scanner.close(); 
+    }
+}
