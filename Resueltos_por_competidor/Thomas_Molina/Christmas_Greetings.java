ac938e7d4c74bfdd0b66c4e4c454ae3864a378a1
thomas08095 <thomas08095@gmail.com>
2026-05-13 21:56:45 -0500

    Add files via upload

---
+++ Resueltos_por_competidor/Thomas_Molina/Christmas_Greetings.java
@@ -0,0 +1,20 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class Christmas_Greetings {
+
+	public static void main(String[] args) {
+		{
+		    int X;
+		    Scanner sc = new Scanner (System.in);
+		    X = sc.nextInt();
+		    if (X==25){
+		        System.out.println("CHRISTMAS");
+		    }
+		    else{
+		        System.out.println("ORDINARY");
+		    }
+		}
+	}
+}
